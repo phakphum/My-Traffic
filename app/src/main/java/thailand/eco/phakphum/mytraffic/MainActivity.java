e0378c62a1866fdf1ac2ad676408dac8346ee38c
phakphuc <phakphuc@gmail.com>
2016-07-03 11:43:00 +0700

    Start Detail Acvitity

--- app/src/main/java/thailand/eco/phakphum/mytraffic/MainActivity.java
+++ app/src/main/java/thailand/eco/phakphum/mytraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -38,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 //Sound Effect
                 // ส้ราง Class ย่อย
                 // กด Ctrl ค้างไว้ใน () เพื่อตรวจสอบ parameter ที่ต้องใส่
-                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.horse);
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.horse);
                 mediaPlayer.start();
 
                 //Web View
@@ -51,6 +52,56 @@ public class MainActivity extends AppCompatActivity {
             }   // onClick
         });
 
+        //Create ListView
+        //1. การทำ Setter array Simple ** ง่ายสุด ธรรมชาติ
+        final int[] iconInts = new int[20];
+        iconInts[0] = R.drawable.traffic_01;
+        iconInts[1] = R.drawable.traffic_02;
+        iconInts[2] = R.drawable.traffic_03;
+        iconInts[3] = R.drawable.traffic_04;
+        iconInts[4] = R.drawable.traffic_05;
+        iconInts[5] = R.drawable.traffic_06;
+        iconInts[6] = R.drawable.traffic_07;
+        iconInts[7] = R.drawable.traffic_08;
+        iconInts[8] = R.drawable.traffic_09;
+        iconInts[9] = R.drawable.traffic_10;
+        iconInts[10] = R.drawable.traffic_11;
+        iconInts[11] = R.drawable.traffic_12;
+        iconInts[12] = R.drawable.traffic_13;
+        iconInts[13] = R.drawable.traffic_14;
+        iconInts[14] = R.drawable.traffic_15;
+        iconInts[15] = R.drawable.traffic_16;
+        iconInts[16] = R.drawable.traffic_17;
+        iconInts[17] = R.drawable.traffic_18;
+        iconInts[18] = R.drawable.traffic_19;
+        iconInts[19] = R.drawable.traffic_20;
+
+        //2. Call Value From xml **รูปแบบการทำงานร่วมกับ ทีม Copy Editor
+        final String[] nameStrings = getResources().getStringArray(R.array.name);
+
+        //3. Call Value From Java ** ต้องทำการสร้าง Data ในไฟล์ Class.java ก่อน
+        // แล้วสร้าง Object **
+        MyData myData = new MyData();   // ** Create object or instance
+        String[] detailStrings = myData.getDetailStrings();
+
+        //Inheriate to MyAdapter
+        MyAdapter myAdapter = new MyAdapter(this, iconInts, nameStrings, detailStrings);
+        trafficListView.setAdapter(myAdapter);
+
+        //Intent to Detail(DetailActivity) When Click
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //การ Intent จาก MainActivity ไป DetailActivity
+                Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+                intent.putExtra("Name", nameStrings[position]);
+                intent.putExtra("Icon", iconInts[position]);
+                intent.putExtra("Index", position);
+                startActivity(intent);  //เปิดหน้าใหม่ DetailActivity ตามที่ Intent มา
+
+            }// onItemClick
+        });
 
     }   //Main Method
 }   //Main Class หรือ คลาสหลัก
---
+++ app/src/main/java/thailand/eco/phakphum/mytraffic/MyData.java
@@ -0,0 +1,17 @@
+package thailand.eco.phakphum.mytraffic;
+
+/**
+ * Created by Phakphum on 03-Jul-16.
+ */
+public class MyData {
+
+    //Explicit
+    private String[] detailStrings = new String[]{"รายละเอียด 1", "รายละเอียด 2", "รายละเอียด 3", "รายละเอียด 4",
+            "รายละเอียด 5", "รายละเอียด 6", "รายละเอียด 7", "รายละเอียด 8", "รายละเอียด 9", "รายละเอียด 10", "รายละเอียด 11",
+            "รายละเอียด 12", "รายละเอียด 13", "รายละเอียด 14", "รายละเอียด 15", "รายละเอียด 16", "รายละเอียด 17", "รายละเอียด 18",
+            "รายละเอียด 19", "รายละเอียด 20"};
+
+    public String[] getDetailStrings() {
+        return detailStrings;
+    }
+}//Main Class
